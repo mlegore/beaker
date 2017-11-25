f541dd096d587e9b1a8ae39bff6a9410b32fb3c0
Michael LeGore <thelegore@gmail.com>
2017-11-25 09:57:32 -0500

    Fix rebuild scripts to rebuild native modules
    Adding electon-rebuild into rebuild process to handle rebuilding node
    native modules into proper node version used by electron. This should
    fix issue with petri not being able to spawn sbot if necessary.

--- tasks/rebuild.js
+++ tasks/rebuild.js
@@ -8,7 +8,7 @@ gulp.task('rebuild-deps', async () => {
   // TODO read electron version
   const cwd = path.join(process.cwd(), 'app')
   console.log(cwd)
-  await run(`npm rebuild --runtime=electron --target=1.7.4 --disturl=https://atom.io/download/atom-shell --build-from-source`, {cwd, shell: true})
+  await run(`electron-rebuild`, {cwd, shell: true})
 })
 
 gulp.task('rebuild', ['rebuild-deps', 'build'])
