9bf269a7999288136ef993287e341a677d6ca25f
Michael LeGore <thelegore@gmail.com>
2017-12-20 22:29:55 -0500

    Add internal api export to framework

--- app/frameworks/index.js
+++ app/frameworks/index.js
@@ -8,6 +8,7 @@ import { Writable } from 'stream';
 import rpc from 'pauls-electron-rpc'
 import {getFrameworkPerm} from '../lib/strings'
 import {queryPermission as queryPerm} from '../background-process/ui/permissions'
+import {internalOnly} from '../../lib/bg/rpc'
 
 function emitterAPIToStream(method) {
   return (...args) => {
@@ -34,7 +35,7 @@ function emitterAPIToStream(method) {
 
 function framework (frameworkName) {
   return {
-    exportAPI (manifest, api, options) {
+    exportInternalAPI (manifest, api) {
       // Extend api to support EventEmitters and Listeners
       api = Object.keys(api).reduce(function (acc, key) {
         acc[key] = manifest[key] === 'emitter' ? emitterAPIToStream(api[key]) : api[key]
@@ -46,7 +47,21 @@ function framework (frameworkName) {
         return acc
       }, {})
 
-      rpc.exportAPI('framework/' + frameworkName, manifest, api, options)
+      rpc.exportAPI('internal-' + frameworkName, manifest, api, internalOnly)
+    },
+    exportAPI (manifest, api) {
+      // Extend api to support EventEmitters and Listeners
+      api = Object.keys(api).reduce(function (acc, key) {
+        acc[key] = manifest[key] === 'emitter' ? emitterAPIToStream(api[key]) : api[key]
+        return acc
+      }, {})
+
+      manifest = Object.keys(manifest).reduce(function (acc, key) {
+        acc[key] = manifest[key] === 'emitter' ? 'readable' : manifest[key]
+        return acc
+      }, {})
+
+      rpc.exportAPI('framework/' + frameworkName, manifest, api)
     },
     async queryPermission (frameworkPerm, sender) {
       var perm = getFrameworkPerm(frameworkName, frameworkPerm)
